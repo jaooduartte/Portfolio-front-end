ac9928b21ecd2ae05b0809641ad5305a3f62d001
João Duarte <jpdx2010@hotmail.com>
2024-12-10 10:46:10 -0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CardProfileComponent } from './components/card-profile/card-profile.component';
+import { DescriptionComponent } from "./components/description/description.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { SkillsComponent } from "./components/skills/skills.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [CardProfileComponent],
+  imports: [CardProfileComponent, DescriptionComponent, HeaderComponent, SkillsComponent],
 })
 export class AppComponent {}
