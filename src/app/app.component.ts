53c31b3597f26d234f9f0b301e39c325a1914d9c
João Duarte <jpdx2010@hotmail.com>
2024-12-10 00:16:23 -0300

    Alterando o design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CardProfileComponent } from './components/card-profile/card-profile.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [CardProfileComponent],
 })
-export class AppComponent {
-  title = 'portfolio';
-}
+export class AppComponent {}
---
+++ src/app/components/card-profile/card-profile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-profile',
+  imports: [],
+  templateUrl: './card-profile.component.html',
+  styleUrl: './card-profile.component.scss'
+})
+export class CardProfileComponent {
+
+}
